eabffb36680b1e39e070b8e40cff2650554dddd3
miftah06 <61154976+miftah06@users.noreply.github.com>
2023-12-16 22:08:11 +0800

    Update subfinder.go

--- subfinder.go
+++ subfinder.go
@@ -3,106 +3,58 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net"
 	"net/http"
 	"os"
-	"strings"
 	"sync"
+	"time"
 )
 
-func getNSRecords(domain string) []string {
-	nsRecords := []string{"ns1", "ns2", "ns3"}
-	var nsIPs []string
-
-	for _, ns := range nsRecords {
-		nsIP, err := lookupNS(domain, ns)
-		if err == nil {
-			nsIPs = append(nsIPs, nsIP)
-		}
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: subdomain_finder <domain> <subdomains_file>")
+		os.Exit(1)
 	}
 
-	return nsIPs
-}
+	domain := os.Args[1]
+	subdomainsFile := os.Args[2]
 
-func lookupNS(domain string, ns string) (string, error) {
-	nsAddress, err := lookupAddress(fmt.Sprintf("%s.%s", ns, domain))
+	subdomains := make(chan string)
+	var wg sync.WaitGroup
+
+	// Read subdomains from the file
+	file, err := os.Open(subdomainsFile)
 	if err != nil {
-		return "", err
-	}
-	return nsAddress, nil
-}
-
-func lookupAddress(host string) (string, error) {
-	ips, err := net.LookupHost(host)
-	if err != nil {
-		return "", err
-	}
-	return ips[0], nil
-}
-
-func scanSubdomains(domain string, output chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	subdomains, err := readLines("subdomains.txt")
-	if err != nil {
-		fmt.Println("Error reading subdomains:", err)
-		return
-	}
-
-	for _, subdomain := range subdomains {
-		url := fmt.Sprintf("http://%s.%s", subdomain, domain)
-		resp, err := http.Get(url)
-		if err == nil && resp.StatusCode == 200 {
-			nsRecords := getNSRecords(domain)
-			output <- fmt.Sprintf("Subdomain found: %s\nNS Records: %s\n\n", url, strings.Join(nsRecords, ", "))
-		}
-	}
-}
-
-func readLines(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
+		fmt.Println("Error reading subdomains file:", err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
-	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-func main() {
-	domains, err := readLines("domains.txt")
-	if err != nil {
-		fmt.Println("Error reading domains:", err)
-		return
-	}
-
-	var wg sync.WaitGroup
-	output := make(chan string)
-
-	for _, domain := range domains {
 		wg.Add(1)
-		go scanSubdomains(domain, output, &wg)
+		go checkSubdomain(domain, scanner.Text(), subdomains, &wg)
 	}
 
 	go func() {
 		wg.Wait()
-		close(output)
+		close(subdomains)
 	}()
 
-	resultFile, err := os.Create("output.txt")
-	if err != nil {
-		fmt.Println("Error creating output file:", err)
-		return
+	for subdomain := range subdomains {
+		fmt.Println(subdomain)
 	}
-	defer resultFile.Close()
+}
 
-	for result := range output {
-		resultFile.WriteString(result)
-		fmt.Print(result)
+func checkSubdomain(domain, subdomain string, subdomains chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	url := fmt.Sprintf("http://%s.%s", subdomain, domain)
+	client := http.Client{
+		Timeout: 2 * time.Second,
 	}
+	resp, err := client.Get(url)
+	if err == nil && resp.StatusCode < 400 {
+		subdomains <- subdomain
+	}
+
+	// You can add additional checks or validation here if needed
 }
