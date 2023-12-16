18684b1bc1a7e99af741b9b80ac86242a7ccf9b2
miftah06 <izharuddinmiftah@gmail.com>
2023-12-16 23:47:35 +0800

    update 2

--- subfinder.py
+++ subfinder.py
@@ -9,14 +9,19 @@ def scan_subdomain(domain):
 
     for subdomain in subdomains:
         url = f"http://{subdomain}.{domain}"
+        result = f"{subdomain}.{domain}"
         try:
             response = requests.get(url)
             if response.status_code == 200:
-                print(f"Subdomain found: {url}")
+                print(f"Subdomain found: {url}" + '\n')
                 domain_results.append(url)
         except requests.RequestException:
             pass
 
+    with open("output.txt", "w") as output_file:
+        for domain_results in result:
+            output_file.write(f"{result}" + '\n')
+            
     return domain_results
 
 def main():
@@ -28,11 +33,6 @@ def main():
     results = pool.map(scan_subdomain, domains)
     pool.close()
     pool.join()
-
-    with open("output.txt", "w") as output_file:
-        for domain_results in results:
-            for result in domain_results:
-                output_file.write(f"Subdomain found: {result}" + '\n')
 
 if __name__ == "__main__":
     main()
