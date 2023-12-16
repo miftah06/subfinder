9e0b033f9d827d6b535b3f97abaa3438f9546f52
miftah06 <61154976+miftah06@users.noreply.github.com>
2023-12-16 22:16:21 +0800

    Update subfinder.go

--- subfinder.go
+++ subfinder.go
@@ -1,18 +1,135 @@
 package main
 
+
+
+import (
+
+	"bufio"
+
+	"fmt"
+
+	"net/http"
+
+	"os"
+
+	"sync"
+
+	"time"
+
+)
+
+
+
+func main() {
+
+	if len(os.Args) != 2 {
+
+		fmt.Println("Usage: subdfinder <domain>")
+
+		os.Exit(1)
+
+	}
+
+
+
+	domain := os.Args[1]
+
+	subdomains := make(chan string)
+
+	var wg sync.WaitGroup
+
+
+
+	// Read common subdomains from a file
+
+	file, err := os.Open("subdomains.txt")
+
+	if err != nil {
+
+		fmt.Println("Error reading subdomains file:", err)
+
+		os.Exit(1)
+
+	}
+
+	defer file.Close()
+
+
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		wg.Add(1)
+
+		go checkSubdomain(domain, scanner.Text(), subdomains, &wg)
+
+	}
+
+
+
+	go func() {
+
+		wg.Wait()
+
+		close(subdomains)
+
+	}()
+
+
+
+	for subdomain := range subdomains {
+
+		fmt.Println(subdomain)
+
+	}
+
+}
+
+
+
+func checkSubdomain(domain, subdomain string, subdomains chan string, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	url := fmt.Sprintf("http://%s.%s", subdomain, domain)
+
+	client := http.Client{
+
+		Timeout: 2 * time.Second,
+
+	}
+
+	resp, err := client.Get(url)
+
+	if err == nil && resp.StatusCode < 400 {
+
+		subdomains <- subdomain
+
+	}
+
+
+
+	// You can add additional checks or validation here if needed
+
+}
+
+
+
+package main
+
 import (
 	"bufio"
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"sync"
 	"time"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: subdomain_finder <domain>")
+		fmt.Println("Usage: subfinder <domain>")
 		os.Exit(1)
 	}
 
