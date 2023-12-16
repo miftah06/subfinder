b2e5bf9ecdbe315cc2e11282769a0c56258f7436
miftah06 <izharuddinmiftah@gmail.com>
2023-12-16 23:22:15 +0800

    update 1

--- status.py
+++ status.py
@@ -1,62 +1,42 @@
-import socket
-import requests
-import numpy as np
-import tornado.gen
-from tornado.ioloop import IOLoop
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+import subprocess
 
-# Fungsi untuk mendapatkan daftar URL dari file teks
-def get_urls_from_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read().splitlines()
-
-# Fungsi untuk mendapatkan alamat IP dari nama host menggunakan dnspython
-def resolve_hostname(hostname):
+def get_dns_info(hostname):
     try:
-        result = socket.gethostbyname(hostname)
-        return result
-    except socket.error as e:
-        print(f"Error resolving {hostname}: {e}")
-        return None
+        # Scanning CNAME
+        cname_result = subprocess.check_output(['nslookup', '-type=CNAME', hostname], universal_newlines=True)
+        cname_values = [line.split(':')[-1].strip() for line in cname_result.splitlines() if 'canonical name' in line.lower()]
+    except subprocess.CalledProcessError:
+        cname_values = None
 
-# Fungsi untuk melakukan HTTP request dan mendapatkan status code dan server DNS
-@tornado.gen.coroutine
-def fetch_url(url):
-    hostname = url.replace("http://", "").split("/")[0]
-    
-    ip_address = resolve_hostname(hostname)
-    if ip_address:
-        try:
-            response = yield AsyncHTTPClient().fetch(HTTPRequest(url, connect_timeout=10, request_timeout=20))
-            status_code = response.code
-        except Exception as e:
-            print(f"Error fetching {url}: {e}")
-            status_code = None
-    else:
-        status_code = None
-    
-    raise tornado.gen.Return((url, ip_address, status_code))
+    try:
+        # Scanning IPv4
+        ipv4_result = subprocess.check_output(['nslookup', '-type=A', hostname], universal_newlines=True)
+        ipv4_addresses = [line.split(':')[-1].strip() for line in ipv4_result.splitlines() if 'address' in line.lower()]
+    except subprocess.CalledProcessError:
+        ipv4_addresses = None
 
-# Fungsi utama untuk melakukan bulk HTTP status code check dengan informasi server DNS
-def bulk_http_status_check(file_path):
-    urls = get_urls_from_file(file_path)
-    
-    # Menggunakan Tornado IOLoop untuk operasi asynchronous
-    loop = IOLoop.current()
-    
-    # Menggunakan numpy untuk shuffle daftar URL agar proses menjadi acak
-    np.random.shuffle(urls)
-    
-    # Melakukan resolve hostname dan HTTP request untuk setiap URL secara asynchronous
-    futures = [fetch_url(f"http://{url}") for url in urls]
+    try:
+        # Scanning IPv6
+        ipv6_result = subprocess.check_output(['nslookup', '-type=AAAA', hostname], universal_newlines=True)
+        ipv6_addresses = [line.split(':')[-1].strip() for line in ipv6_result.splitlines() if 'address' in line.lower()]
+    except subprocess.CalledProcessError:
+        ipv6_addresses = None
 
-    # Menunggu semua proses selesai
-    results = loop.run_sync(lambda: tornado.gen.multi(futures))
+    return cname_values, ipv4_addresses, ipv6_addresses
 
-    # Menampilkan hasil
-    for url, ip_address, status_code in results:
-        print(f"{url}: DNS {ip_address}, Status Code {status_code}")
+def bulk_dns_info(file_path, output_file="scan_result.txt"):
+    with open(file_path, 'r') as file:
+        urls = file.read().splitlines()
+
+    with open(output_file, 'w') as output:
+        for url in urls:
+            hostname = url.replace("http://", "").split("/")[0]
+            cname_values, ipv4_addresses, ipv6_addresses = get_dns_info(hostname)
+
+            result_line = f"{url}: CNAME {cname_values}, IPv4 {ipv4_addresses}, IPv6 {ipv6_addresses}\n"
+            output.write(result_line)
+            print(result_line)
 
 if __name__ == "__main__":
     file_path = "hasil.txt"  # Ganti dengan path file yang berisi daftar URL
-    bulk_http_status_check(file_path)
+    bulk_dns_info(file_path)
