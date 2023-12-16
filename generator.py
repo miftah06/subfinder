6dad62a0323237e77b6966bc8aafdfeb2d7a4860
miftah06 <izharuddinmiftah@gmail.com>
2023-12-16 23:31:05 +0800

    update 1

---
+++ generator.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+def generate_random_subdomains(num_subdomains):
+    subdomains = []
+    for _ in range(num_subdomains):
+        subdomain_length = random.randint(1, 5)  # Ganti batasan panjang subdomain sesuai kebutuhan
+        subdomain = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(subdomain_length))
+        subdomains.append(subdomain)
+    return subdomains
+
+def save_to_file(subdomains, file_path="subdomains.txt"):
+    with open(file_path, 'w') as file:
+        for subdomain in subdomains:
+            file.write(f"{subdomain}\n")  # Ganti dengan domain yang sesuai
+
+if __name__ == "__main__":
+    num_subdomains = 1000
+    subdomains = generate_random_subdomains(num_subdomains)
+    save_to_file(subdomains)
+    print(f"{num_subdomains} subdomains telah disimpan dalam subdomains.txt")
