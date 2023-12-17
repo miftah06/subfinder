724e1a87e8404c778befcacaeec004d5aacde704
miftah06 <izharuddinmiftah@gmail.com>
2023-12-17 20:04:02 +0800

    update 1

--- status.py
+++ status.py
@@ -1,4 +1,29 @@
 import subprocess
+import pandas as pd
+import numpy as np
+import requests
+
+def get_status_code(url):
+    try:
+        response = requests.get(url, timeout=5)
+        return response.status_code
+    except requests.exceptions.RequestException as e:
+        return f"Error: {e}"
+
+def check_status_codes(urls):
+    status_codes = {}
+    for url in urls:
+        if not url.startswith("http://") and not url.startswith("https://"):
+            url = "https://" + url
+        status_code = get_status_code(url)
+        status_codes[url] = status_code
+    print(url,":", status_code)    
+    return status_codes
+
+def read_urls_from_file(file_path):
+    with open(file_path, "r") as file:
+        urls = file.read().splitlines()
+    return urls
 
 def get_dns_info(hostname):
     try:
@@ -37,6 +62,19 @@ def bulk_dns_info(file_path, output_file="scan_result.txt"):
             output.write(result_line)
             print(result_line)
 
-if __name__ == "__main__":
+def main():
     file_path = "hasil.txt"  # Ganti dengan path file yang berisi daftar URL
     bulk_dns_info(file_path)
+    file_path = "hasil.txt"
+    urls = read_urls_from_file(file_path)
+
+    status_codes = check_status_codes(urls)
+
+    with open("status_results.txt", "w") as result_file:
+        for url, status_code in status_codes.items():
+            result_file.write(f"{url}: {status_code}\n")
+            
+
+if __name__ == "__main__":
+    main()
+
