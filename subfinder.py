889444b27c1731ceee30e4d8b5e915ee9cf54ab9
miftah06 <izharuddinmiftah@gmail.com>
2023-12-18 11:32:10 +0800

    update 1

--- subfinder.py
+++ subfinder.py
@@ -4,28 +4,32 @@ from multiprocessing.dummy import Pool as ThreadPool
 def scan_subdomain(domain):
     subdomains = []
     domain_results = []
+
     with open("subdomains.txt", "r") as subdomain_file:
         subdomains = subdomain_file.read().splitlines()
 
     for subdomain in subdomains:
         url = f"https://{subdomain}.{domain}"
-        result = f"{subdomain}.{domain}"
         try:
             response = requests.get(url)
-            if response.status_code == 200:
-                print(f"Subdomain found: {url}" + '\n')
+
+            if response.status_code in [200, 301, 400, 409, 502, 401]:
+                server_info = response.headers.get('Server', 'N/A')
+                print(f"Subdomain found: {url} | Status Code: {response.status_code} | Server: {server_info}\n")
                 domain_results.append(url)
+
         except requests.RequestException:
             pass
 
     with open("output.txt", "w") as output_file:
-        for domain_results in result:
-            output_file.write(f"{result}" + '\n')
+        for result in domain_results:
+            output_file.write(f"{result}\n")
             
     return domain_results
 
 def main():
     domains = []
+
     with open("domains.txt", "r") as domain_file:
         domains = domain_file.read().splitlines()
 
